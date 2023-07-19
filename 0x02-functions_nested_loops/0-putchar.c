e0331c73e0b606008d5b25ced3f4c0ca46a55a22
Habtamuay <habwoine@gmail.com>
2023-07-19 02:26:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints "putchar" followed by a new line.
+ * main - Prints _putchar followed by a new line.
  *
  * Return: Always 0.
  */
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
